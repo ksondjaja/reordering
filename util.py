f641fad4360258ba1179c95c51e33f82180863cc
ksondjaja <52758267+ksondjaja@users.noreply.github.com>
2020-10-25 01:24:30 -0400

    Update util.py

--- util.py
+++ util.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 import os
 
-engine = create_engine('mysql://root:kontravoid@localhost/reorder')
+engine = create_engine(os.getenv("DATABASE_URL"))
 db = engine.connect()
 
 def getCategories(table):
